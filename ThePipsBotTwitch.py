b8aa69be4172db6f4b04fd41083b85e323b85825
Korpip <thomas.shawn.milne@gmail.com>
2020-04-11 11:54:29 -0400

    Create ThePipsBotTwitch.py

---
+++ ThePipsBotTwitch.py
@@ -0,0 +1,6 @@
+#config.py
+TMI_TOKEN="Get your oath key"
+CLIENT_ID="get your client id"
+BOT_NICK="ThePipsBot"
+BOT_PREFIX="!" #The prefix that you want to use to denote a chatbot command.
+CHANNEL=["pipsname"] #This can be a list with multiple channels to connect to.
