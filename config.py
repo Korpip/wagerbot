fd994a736f91cb3204d6197a3ee43b6d7b25476e
Korpip <thomas.shawn.milne@gmail.com>
2020-04-11 21:36:29 -0400

    Update and rename ThePipsBotTwitch.py to config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #config.py
 TMI_TOKEN="Get your oath key"
 CLIENT_ID="get your client id"
-BOT_NICK="ThePipsBot"
+BOT_NICK="Bot name here"
 BOT_PREFIX="!" #The prefix that you want to use to denote a chatbot command.
-CHANNEL=["pipsname"] #This can be a list with multiple channels to connect to.
+CHANNEL=["bookienamehere"] #This can be a list with multiple channels to connect to.
